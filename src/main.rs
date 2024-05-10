96be6080eae78df7bc9c99b0a509610eb9356dd0
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-05-10 21:36:04 +0000

    capture video feeds from camers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{closure::Closure, JsCast, JsValue};
 use wasm_bindgen_futures::{spawn_local, JsFuture};
-use web_sys::{js_sys::Boolean, *};
+use web_sys::{
+    js_sys::{Array, Boolean, JsString, Reflect},
+    *,
+};
 use yew::prelude::*;
+
+static VIDEO_CODEC: &str = "vp09.00.10.08";
+const VIDEO_HEIGHT: i32 = 1280i32;
+const VIDEO_WIDTH: i32 = 720i32;
 
 #[function_component(Producer)]
 fn producer() -> Html {
@@ -26,6 +33,57 @@ fn producer() -> Html {
             .unwrap()
             .unchecked_into::<MediaStream>();
         video_element.set_src_object(Some(&device));
+
+        let video_track = Box::new(
+            device
+                .get_video_tracks()
+                .find(&mut |_: JsValue, _: u32, _: Array| true)
+                .unchecked_into::<VideoTrack>(),
+        );
+
+        let error_handler = Closure::wrap(Box::new(move |e: JsValue| {
+            console::log_1(&JsString::from("on error"));
+            console::log_1(&e);
+        }) as Box<dyn FnMut(JsValue)>);
+        let output_handler = Closure::wrap(Box::new(move |chunk: JsValue| {
+            // let video_chunk = chunk.unchecked_into::<EncodedVideoChunk>();
+            console::log_1(&chunk);
+        }) as Box<dyn FnMut(JsValue)>);
+
+        let video_encoder_init = VideoEncoderInit::new(
+            error_handler.as_ref().unchecked_ref(),
+            output_handler.as_ref().unchecked_ref(),
+        );
+        let video_encoder = VideoEncoder::new(&video_encoder_init).unwrap();
+        let video_encoder_config =
+            VideoEncoderConfig::new(&VIDEO_CODEC, VIDEO_HEIGHT as u32, VIDEO_WIDTH as u32);
+        video_encoder.configure(&video_encoder_config);
+
+        let processor = MediaStreamTrackProcessor::new(&MediaStreamTrackProcessorInit::new(
+            &video_track.unchecked_into::<MediaStreamTrack>(),
+        ))
+        .unwrap();
+
+        let reader = processor
+            .readable()
+            .get_reader()
+            .unchecked_into::<ReadableStreamDefaultReader>();
+
+        loop {
+            let result = JsFuture::from(reader.read()).await.map_err(|e| {
+                console::log_1(&e);
+            });
+            match result {
+                Ok(js_frame) => {
+                    let frame = Reflect::get(&js_frame, &JsString::from("value"))
+                        .unwrap()
+                        .unchecked_into::<VideoFrame>();
+                    video_encoder.encode(&frame);
+                    frame.close();
+                }
+                Err(err) => console::log_1(&JsString::from(format!("Error: {:?}", err))),
+            }
+        }
     });
 
     html! {
